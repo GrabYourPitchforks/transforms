11b8fd0d1277288f40041cbf1dda818ae16ace1c
Levi Broderick <levib@microsoft.com>
2017-09-29 16:42:27 -0700

    Add sample program

---
+++ Transforms/StringView.cs
@@ -0,0 +1,15 @@
+﻿namespace System
+{
+    public ref struct StringView
+    {
+        public StringView(String value);
+        public StringView(String value, int offset, int count);
+        public StringView(StringView value);
+        public StringView(StringView value, int offset, int count);
+        public StringView(ReadOnlySpan<char> value);
+
+        // Equals, IndexOf, SubStr, iterators, and all forms of goodness
+
+        public ReadOnlySpan<char> AsSpan();
+    }
+}
