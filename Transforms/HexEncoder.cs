bb74abb19e657c2a80cfc73dfa0a5743de0bb3b5
Levi Broderick <levib@microsoft.com>
2017-10-05 14:21:00 -0700

    Implement hex encoder

--- Transforms/HexEncoder.cs
+++ Transforms/HexEncoder.cs
@@ -1,33 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Numerics;
 
 namespace Transforms
 {
     public class HexEncoder : IStatelessTransform<byte, char>, IStatelessTransform<byte, Utf8Char>
     {
-        public bool CanTransformInPlace => throw new NotImplementedException();
+        private readonly bool _useLowercase;
 
-        public int GetMaxOutputElementCount(int numInputElements)
+        public HexEncoder()
+            : this(useLowercase: false)
         {
-            throw new NotImplementedException();
         }
+
+        public HexEncoder(bool useLowercase)
+        {
+            _useLowercase = useLowercase;
+        }
+
+        public bool CanTransformInPlace { get; } = false;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static uint GetHexValue(uint nibble, uint addendWhenAlpha)
+        {
+            Debug.Assert(nibble < 16);
+            return nibble + ((nibble < 10) ? '0' : addendWhenAlpha);
+        }
+
+        public int GetMaxOutputElementCount(int numInputElements) => (int)checked((uint)numInputElements * 2);
 
         [return: MustInspect]
         public TransformStatus Transform(ReadOnlySpan<byte> input, Span<char> output, bool isFinalChunk, out int numElementsConsumed, out int numElementsWritten)
         {
-            throw new NotImplementedException();
+            return TransformCommon<char>(input, output, isFinalChunk, out numElementsConsumed, out numElementsWritten);
         }
 
         [return: MustInspect]
         public TransformStatus Transform(ReadOnlySpan<byte> input, Span<Utf8Char> output, bool isFinalChunk, out int numElementsConsumed, out int numElementsWritten)
         {
-            throw new NotImplementedException();
+            return TransformCommon<Utf8Char>(input, output, isFinalChunk, out numElementsConsumed, out numElementsWritten);
+        }
+
+        private TransformStatus TransformCommon<TOut>(ReadOnlySpan<byte> input, Span<TOut> output, bool isFinalChunk, out int numElementsConsumed, out int numElementsWritten)
+        {
+            int elementsToConsumeCount = Math.Min(input.Length, output.Length / 2);
+
+            TransformCore(
+                   input: ref input.DangerousGetPinnableReference(),
+                   inputLength: elementsToConsumeCount,
+                   output: ref Unsafe.Add(ref Unsafe.As<TOut, byte>(ref output.DangerousGetPinnableReference()), (BitConverter.IsLittleEndian) ? 0 : (Unsafe.SizeOf<TOut>() - 1)),
+                   outputStride: Unsafe.SizeOf<TOut>(),
+                   addendWhenAlpha: (_useLowercase) ? (uint)('a' - 10) : (uint)('A' - 10));
+
+            numElementsConsumed = elementsToConsumeCount;
+            numElementsWritten = elementsToConsumeCount * 2;
+            return (numElementsConsumed == input.Length) ? TransformStatus.FullyCompleted : TransformStatus.PartiallyCompleted;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static void TransformCore(ref byte input, int inputLength, ref byte output, int outputStride, uint addendWhenAlpha)
+        {
+            for (int i = 0; i < inputLength; i++)
+            {
+                uint thisByte = Unsafe.Add(ref input, i);
+                Unsafe.Add(ref output, outputStride * (2 * i)) = (byte)GetHexValue(thisByte >> 4, addendWhenAlpha);
+                Unsafe.Add(ref output, outputStride * (2 * i + 1)) = (byte)GetHexValue(thisByte & 0xf, addendWhenAlpha);
+            }
         }
 
         public bool TryGetTransformedElementCount(ReadOnlySpan<byte> input, bool isFinalChunk, out int numOutputElements)
         {
-            throw new NotImplementedException();
+            // We can't report an output element count that overflows Int32.
+            // This is ok if this isn't the final chunk, since the caller will just invoke this method in a loop.
+            // If this is the final chunk, the caller expects us to give him an exact count, which we can't do.
+
+            long actualOutputCount = input.Length * 2 /* output elements per input element */;
+            numOutputElements = (int)Math.Min(actualOutputCount, Int32.MinValue);
+            return (!isFinalChunk || (numOutputElements == actualOutputCount));
         }
     }
 }
--- Transforms/Program.cs
+++ Transforms/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using Transforms;
 
 public static class Program
 {
     public static void Main()
     {
-        
+        byte[] b = { 1, 2, 3, 4, 5, 0xfe };
+        char[] cs = new char[40];
+
+        new HexEncoder(true).Transform(b, cs, true, out _, out _);
     }
 }
