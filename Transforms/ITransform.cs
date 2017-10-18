f6006b29d472e7481e9e980929adba3ad58cd4a2
Levi Broderick <levib@microsoft.com>
2017-10-18 15:02:52 -0700

    Add ITransform interface and devdoc

---
+++ Transforms/ITransform.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Transforms
+{
+    /// <summary>
+    /// Provides a mechanism for transforming data from one representation to another in
+    /// a stateless manner. Data is provided to the <see cref="TryTransform(ReadOnlySpan{TIn}, Span{TOut}, bool, out int, out int)"/>
+    /// method in chunks.
+    /// </summary>
+    /// <typeparam name="TIn">The source type representing the input data to be transformed.</typeparam>
+    /// <typeparam name="TOut">The destination type representing the transformed output data.</typeparam>
+    public interface ITransform<TIn, TOut>
+    {
+        /// <summary>
+        /// Determines the maximum number of output elements that can result from transforming an input of the given length.
+        /// </summary>
+        /// <param name="inputLength">The number of input elements.</param>
+        /// <returns>The maximum number of output elements that could result from the transform.</returns>
+        /// <remarks>
+        /// This method throws an exception if <paramref name="inputLength"/> is negative or if the return
+        /// value cannot fit in a <see cref="long"/>.
+        /// </remarks>
+        long GetMaxOutputLength(long inputLength);
+
+        /// <summary>
+        /// Determines the exact size of the buffer required to hold all of the output which would result from a call to
+        /// <see cref="TryTransform(ReadOnlySpan{TIn}, Span{TOut}, bool, out int, out int)"/> with the specified input.
+        /// </summary>
+        /// <param name="input">The source of the data to transform.</param>
+        /// <param name="isFinalChunk"><see langword="true"/> if <paramref name="input"/> contains the last of the data to
+        /// transform. <see langword="false"/> if the caller will provide more data in another call.</param>
+        /// <param name="outputLength">
+        /// If this method returns <see langword="true"/>, contains the number of output elements.
+        /// If this method returns <see langword="false"/>, undefined.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if the input is well-formed and if the exact length of the required output buffer
+        /// can be determined. <see langword="false"/> if the input is not well-formed or if the required output buffer
+        /// length cannot fit into a <see cref="long"/>.
+        /// </returns>
+        bool TryGetOutputLength(ReadOnlySpan<TIn> input, bool isFinalChunk, out long outputLength);
+
+        /// <summary>
+        /// Transforms elements from a source buffer, placing the transformed elements in a destination buffer.
+        /// </summary>
+        /// <param name="input">The source of the data to transform.</param>
+        /// <param name="output">
+        /// The destination buffer in which to place transformed data.
+        /// If this method returns <see langword="true"/>, then <paramref name="numWritten"/> elements
+        /// were written to the destination buffer. If this method returns <see langword="false"/>,
+        /// the contents of this buffer are undefined.
+        /// </param>
+        /// <param name="isFinalChunk"><see langword="true"/> if <paramref name="input"/> contains the last of the data to
+        /// transform. <see langword="false"/> if the caller will provide more data in another call.</param>
+        /// <param name="numConsumed">
+        /// If this method returns <see langword="true"/>, contains the number of elements from <paramref name="numConsumed"/>
+        /// which were successfully transformed. This may be less than the total number of input elements.
+        /// If this method returns <see langword="false"/>, undefined.
+        /// </param>
+        /// <param name="numWritten">
+        /// If this method returns <see langword="true"/>, contains the number of elements written to <paramref name="output"/>.
+        /// This may be less than the total length of the output buffer.
+        /// If this method returns <see langword="false"/>, undefined.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if the input data is well-formed and zero or more elements were tranformed
+        /// from the source buffer to the destination buffer. <see langword="false"/> if the input data is not
+        /// well-formed.
+        /// </returns>
+        /// <remarks>
+        /// It is possible for this method to return <see langword="true"/> even if no data was transformed,
+        /// such as in the case where the input buffer or the output buffer is not large enough to allow even
+        /// a partial transformation. In this case the caller should provide a larger input or output buffer
+        /// and attempt the call again. If this method returns <see langword="false"/>, the input data is not
+        /// well-formed and the caller should not attempt the call again with the same input data, as it will
+        /// never succeed.
+        /// </remarks>
+        bool TryTransform(ReadOnlySpan<TIn> input, Span<TOut> output, bool isFinalChunk, out int numConsumed, out int numWritten);
+    }
+}
---
+++ Transforms/ITransformInPlace.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Transforms
+{
+    /// <summary>
+    /// Represents an <see cref="ITransform{T, T}"/> that may allow transforming
+    /// data in-place within a single buffer.
+    /// </summary>
+    /// <typeparam name="T">The type of the data to undergo transformation.</typeparam>
+    public interface ITransformInPlace<T> : ITransform<T, T>
+    {
+        /// <summary>
+        /// <see langword="true"/> if this transform allows transforming data in-place,
+        /// <see langword="false"/> if transforming data in-place is not supported.
+        /// </summary>
+        bool CanTransformInPlace { get; }
+
+        /// <summary>
+        /// Transforms elements within a buffer. The total number of output elements
+        /// may be fewer than or greater than the total number of input elements.
+        /// </summary>
+        /// <param name="data">
+        /// A buffer containing the data to be transformed.
+        /// The transformation will start from the first element in the buffer.
+        /// </param>
+        /// <param name="inputLength">The number of elements in <paramref name="data"/> to be transformed.</param>
+        /// <param name="outputLength">
+        /// If this method returns <see langword="true"/>, contains the number of transformed
+        /// elements written to the buffer. The output starts at the first element in the buffer.
+        /// If this method returns <see langword="false"/>, undefined.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if the input data is well-formed and the buffer is large enough
+        /// to hold the final result of transforming all input elements.
+        /// <see langword="false"/> if the input data is not well-formed or if the buffer is not
+        /// large enough to allow for a total transformation of all input elements.
+        /// </returns>
+        bool TryTransformInPlace(Span<T> data, int inputLength, out int outputLength);
+    }
+}
